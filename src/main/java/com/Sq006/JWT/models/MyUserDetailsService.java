96d79ab60f13fccd099e6a9523c803007e320006
Daro <darogadibia@gmail.com>
2021-03-09 10:42:56 +0100

    make changes to hello world controller

--- src/main/java/com/Sq006/JWT/models/MyUserDetailsService.java
+++ src/main/java/com/Sq006/JWT/models/MyUserDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -16,6 +17,7 @@ public class MyUserDetailsService implements UserDetailsService {
     UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         return MyUserDetails.build(userRepository.findByUsername(s)
                 .orElseThrow(()-> new UsernameNotFoundException("No such user found")));
